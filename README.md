86089b91a9bd9746359f4b68eec9a08822b0efc1
GyeongSam <rud7tka@gmail.com>
2023-05-19 12:03:50 +0900

    C#파일 업로드

--- README.md
+++
@@ -1,1 +0,0 @@
-https://navy-taleggio-29f.notion.site/389648c9cd0c44e49681f2819b69f191
--- auth-server/src/main/java/com/ssafy/raid/auth/dto/ResultCode.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.raid.auth.dto;
-
-public class ResultCode {
-	public static final int AuthIsIncomplete = 0;
-	public static final int AuthIsComplete = 1;
-	public static final int Failed = 2;
-	public static final int InvalidParameters = 3;
-	
-}
